8188cb52b06162d84019f687fba05c74dbe7efc7
dajkagabi <dajkagabi01@gmail.com>
2023-03-08 14:04:28 +0100

    Initial commit Generated by create-expo-app 1.3.2.

--- App.js
+++ App.js
@@ -1,11 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ToastAndroid, TouchableOpacity } from 'react-native';
+import React, { PureComponent } from 'react'
+
+
 
 export default function App() {
+
+  const showToast = () => {
+    //Console OUTPUT
+    console.log("Toast clicked :D")
+    ToastAndroid.show(
+      //Button show name
+      "Hello Toast",
+      ToastAndroid.SHORT,
+      ToastAndroid.BOTTOM
+    )
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Hello React-Native!</Text>
       <StatusBar style="auto" />
+
+      <TouchableOpacity style={styles.button}
+        onPress={() => showToast()}>
+        <Text style={{ color: "#fff" }}>Press me!</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -17,4 +38,23 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  text: {
+    color: "#191970",
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    padding: 50
+  },
+
+  button: {
+    borderRadius: 10,
+    backgroundColor: "#000080",
+    width: 150,
+    height: 50,
+    justifyContent: "center",
+    alignItems: "center",
+  }
+
+
 });
